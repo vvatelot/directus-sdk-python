678c3c3d83c72018b1668114d26aeea0fa31beaf
Jonas L <jooola@users.noreply.github.com>
2020-03-20 17:59:03 +0100

    Format code using Black (#5)

--- directus_api/directus.py
+++ directus_api/directus.py
@@ -1,9 +1,9 @@
 import requests, json
 
-class DirectusClient:
 
+class DirectusClient:
     def __init__(self, url, project, email=None, password=None):
-        self.url = url + '/' + project
+        self.url = url + "/" + project
         self.email = email
         self.password = password
         self.project = project
@@ -11,16 +11,13 @@ class DirectusClient:
 
         if email is not None and password is not None:
 
-            payload = {
-                "email": email,
-                "password": password
-            }
+            payload = {"email": email, "password": password}
 
-            combined_url = self.url + '/auth/authenticate'
+            combined_url = self.url + "/auth/authenticate"
             r = requests.post(combined_url, data=payload)
-            data = r.json() 
+            data = r.json()
 
-            self.access = data['data']["token"]
+            self.access = data["data"]["token"]
 
     def get_items_list(self, collection):
         return requests.get(self.url + "/items/" + collection).json()
@@ -33,7 +30,7 @@ class DirectusClient:
 
     def get_files_list(self):
         headers = {"Authorization": "Bearer " + self.access}
-        return requests.get(self.url + "/files", headers=headers).json() 
+        return requests.get(self.url + "/files", headers=headers).json()
 
     def get_file(self, id):
         headers = {"Authorization": "Bearer " + self.access}
